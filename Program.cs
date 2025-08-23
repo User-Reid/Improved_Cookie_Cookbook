006cc03559102d89adccc9d9dce1946309088e1d
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-23 17:06:19 -0500

    created the interface of the general run of the application. I have laid down the template.

--- Program.cs
+++ Program.cs
@@ -3,8 +3,46 @@ cookiesRecipesApp.Run();
 
 public class CookiesRecipesApp
 {
+  private readonly RecipesRepository _recipesRepository;
+  private readonly RecipesUserInteraction _recipesUserInteraction;
+
+  public CookiesRecipesApp(RecipesRepository recipesRepository, RecipesUserInteraction recipesUserInteraction)
+  {
+    _recipesRepository = recipesRepository;
+    _recipesUserInteraction = recipesUserInteraction;
+  }
+
   public void Run()
   {
-    
+    var allRecipes = _recipesRepository.Read(filePath);
+    _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+    _recipesUserInteraction.PromptToCreateRecipe();
+
+    var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+
+    if (ingredients.Count > 0)
+    {
+      var recipes = new Recipe(ingredients);
+      allRecipes.Add(recipe);
+      _recipesRepository.Write(filePath, allRecipes);
+
+      _recipesUserInteraction.ShowMessage("Recipe added:");
+      _recipesUserInteraction.ShowMessage(recipe.ToString());
+    }
+    else
+    {
+      _recipesUserInteraction.ShowMessage("No ingredients have been selected.\n Recipe will not be saved.");
+    }
   }
+}
+
+public class RecipesUserInteraction
+{
+
+}
+
+public class RecipesRepository
+{
+  
 }
