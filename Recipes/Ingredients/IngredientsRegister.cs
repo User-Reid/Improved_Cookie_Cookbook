4ce89d04976fa5dbdee87908e43bd7a982348469
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-08 15:47:54 -0500

    further refactoring :O

--- Recipes/Ingredients/IngredientsRegister.cs
+++ Recipes/Ingredients/IngredientsRegister.cs
@@ -14,6 +14,25 @@ namespace Improved_Cookie_Cookbook.Recipes.Ingredients
     new CocoaPowder()
   };
 
-    public Ingredient GetById(int id) => All.First(recipe => recipe.Id == id);
+    public Ingredient GetById(int id)
+    {
+      var allIngredientsWithGivenId = All
+      .Where(ingredient => ingredient.Id == id);
+
+      if (allIngredientsWithGivenId.Count() > 1)
+      {
+        throw new InvalidOperationException(
+          $"More than one ingredients have ID equal to {id}."
+        );
+      }
+
+      // if (All.Select(ingredient => ingredient.Id).Distinct().Count() != All.Count()) {
+      //     throw new InvalidOperationException(
+      //     $"Some ingredients have duplicated IDs"
+      //   );
+      // }
+
+      return allIngredientsWithGivenId.FirstOrDefault();
+    }
 }
 }
